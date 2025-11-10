5803cc8fb6f4c246268d0e9a809965ffd185028b
letcarmo86953-tech <letcarmo86953@gmail.com>
2025-11-10 19:13:42 +0000

    mano

--- MVC/app/config/database.php
+++ MVC/app/config/database.php
@@ -1,17 +1,17 @@
 <?php
 
-function getConnection(): PDO{
-    $host = "localhost";
-    $db = "biblioteca_filmes";
+function getConnection(): PDO {
+$host = "127.0.0.1";
+    $db   = "biblioteca_filmes";
+    $port = 3306;
     $user = "root";
     $pass = "leticia";
 
     try {
-        $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     } catch (PDOException $e) {
         die("Erro de conexão: " . $e->getMessage());
     }
-
 }
